88476a0f29341bb64456a228c19860c3f9df5a0b
Naeem009 <m.n.shahzad@gmail.com>
2024-05-12 16:52:30 +0500

    updates page.tsx
    main page.tsx updates

--- nextjscounter/src/app/page.tsx
+++ nextjscounter/src/app/page.tsx
@@ -1,22 +1,29 @@
-import Image from "next/image";
+"use client"
 import { useState } from "react";
-import { ClientReferenceManifestPlugin } from "../../node_modules/next/dist/build/webpack/plugins/flight-manifest-plugin";
 
 export default function Home() {
-  return (
-      let number = 1;
+  let number = 1;
       const [count, setCount] = useState(0);
       const increment = () => {
         setCount(count + 1);
       };
-
-      const dcrement = ()=>{
+      const decrement = () => {
         setCount(count - 1);
-      }
-      <h1 className="flex flex-col w-full items-center justify-center">
-        +
+      };
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center px-15">
+    <h1 className="text-4xl flex justify-center items-center font-bold text-black">
+        Counter
       </h1>
-
-    
+      <div className="flex justify-center items-center">
+        <button className="text-9xl font-bold text-black" onClick={decrement}>
+          -
+        </button>
+        <h1 className="text-9xl font-bold text-black">{count}</h1>
+        <button className="text-9xl font-bold text-black" onClick={increment}>
+          +
+        </button>
+      </div>
+    </main>
   );
 }
